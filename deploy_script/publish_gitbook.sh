0e66a387c469084c4925765ee85ba1a0692b9908
aolite <aitorcorchero@gmail.com>
2017-04-08 13:41:38 +0200

    new sh file
    new sh file

--- deploy_script/publish_gitbook.sh
+++ deploy_script/publish_gitbook.sh
@@ -1,55 +1,27 @@
-# abort the script if there is a non-zero error
-set -e
+# install the plugins and build the static site
+gitbook install && gitbook build
 
-# show where we are on the machine
-pwd
+# checkout to the gh-pages branch
+git checkout gh-pages
 
-remote=$(git config remote.origin.url)
+# pull the latest updates
+git pull origin gh-pages --rebase
 
-siteSource=`pwd`
+# copy the static site files into the current directory.
+cp -R _book/* .
 
-echo $siteSource
+# remove 'node_modules' and '_book' directory
+git clean -fx node_modules
+git clean -fx _book
 
-if [ ! -d "$siteSource" ]
-then
-    echo "Usage: $0 <site source dir>"
-    exit 1
-fi
+# add all files
+git add .
 
-# make a directory to put the gp-pages branch
-mkdir gh-pages-branch
-cd gh-pages-branch
+# commit
+git commit -a -m "Update docs"
 
-# now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
-git init
-git remote add --fetch origin "$remote"
+# push to the origin
+git push origin gh-pages
 
-# switch into the the gh-pages branch
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
-then
-    git checkout gh-pages
-    # delete any old site as we are going to replace it
-    # Note: this explodes if there aren't any, so moving it here for now
-    git rm -rf .
-else
-    git checkout --orphan gh-pages
-fi
-
-# copy over or recompile the new site
-cp -a "${siteSource}/docs/_book/." .
-
-# stage any changes and new files
-git add -A
-# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
-# and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages > /dev/null 2>&1
-
-# go back to where we started and remove the gh-pages git repo we made and used
-# for deployment
-cd ..
-rm -rf gh-pages-branch
-
-echo "Finished Deployment!"
+# checkout to the master branch
+git checkout master
